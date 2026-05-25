9f8970febee3da03949de45101274adcf17a29d0
MyeongSeokKo <kms86989408@gmail.com>
2026-05-25 17:22:30 +0900

    feat : train 구현

--- src/activations.py
+++ src/activations.py
@@ -27,7 +27,6 @@ class ReLU:
             x와 같은 shape. x > 0인 위치만 원래 값을 유지합니다.
         """
         # TODO: x > 0 위치를 self.mask에 저장하고, 음수/0 위치는 0으로 바꾸세요.
-        # raise NotImplementedError("ReLU.forward를 구현하세요.")
 
         self.mask = (x <= 0)
         out = x.copy()
@@ -44,7 +43,6 @@ class ReLU:
             ReLU 입력 x에 대한 gradient. forward 때 x <= 0이었던 위치는 0입니다.
         """
         # TODO: forward에서 저장한 self.mask를 이용해 gradient가 흐를 위치만 남기세요.
-        # raise NotImplementedError("ReLU.backward를 구현하세요.")
 
         dout[self.mask] = 0
         dx = dout
